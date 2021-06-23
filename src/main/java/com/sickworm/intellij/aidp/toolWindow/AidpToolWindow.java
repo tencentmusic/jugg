4c13c118f8b96ea574976d26b27e92986ef6a2e3
sickworm <ch.operation@gmail.com>
2021-06-24 00:40:12 +0800

    [feature] ide compatible

--- src/main/java/com/sickworm/intellij/aidp/toolWindow/AidpToolWindow.java
+++ src/main/java/com/sickworm/intellij/aidp/toolWindow/AidpToolWindow.java
@@ -42,7 +42,7 @@ public class AidpToolWindow {
   }
 
   public void apply() {
-    append("apply!!6", JBColor.RED);
+    append("apply!!", JBColor.RED);
     AidpManager manager = AidpManager.Companion.getInstance(project);
     if (manager == null) {
       return;
