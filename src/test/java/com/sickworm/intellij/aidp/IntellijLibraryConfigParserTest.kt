1cfd39f245849a1bbb11f4908eaacfad7587bfc9
sickworm <ch.operation@gmail.com>
2021-08-12 13:50:50 +0800

    [test] improve assert

--- src/test/java/com/sickworm/intellij/aidp/IntellijLibraryConfigParserTest.kt
+++ src/test/java/com/sickworm/intellij/aidp/IntellijLibraryConfigParserTest.kt
@@ -3,6 +3,8 @@ package com.sickworm.intellij.aidp
 import com.sickworm.intellij.aidp.project.IntellijLibraryConfigParser
 import org.junit.Test
 import java.io.File
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
 import kotlin.test.assertTrue
 
 class IntellijLibraryConfigParserTest {
@@ -10,8 +12,8 @@ class IntellijLibraryConfigParserTest {
     @Test
     fun loadLibraryConfig() {
         val result = loadLibraryConfigInTest()
-        assertTrue(result != null)
-        assertTrue(result.size == 50)
+        assertNotNull(result)
+        assertEquals(50, result.size)
         result.forEach {
             println("file: $it")
             assertTrue(File(it).exists())
