22f76467452a45b689ab7baae54657b8307ae0ae
sickworm <ch.operation@gmail.com>
2021-10-18 23:49:30 +0800

    [test] refactor

--- src/test/java/com/sickworm/intellij/jugg/JuggManagerTest.kt
+++ src/test/java/com/sickworm/intellij/jugg/JuggManagerTest.kt
@@ -17,7 +17,10 @@ import com.sickworm.intellij.jugg.toolWindow.JuggLogger
 import org.junit.Before
 import org.junit.Test
 import java.io.File
-import java.util.concurrent.*
+import java.util.concurrent.ExecutionException
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Future
+import java.util.concurrent.TimeUnit
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
@@ -138,96 +141,6 @@ class MockDeployTargetManager(project: Project): DeployTargetManager(project) {
 
     override fun getApks(): List<ApkInfo> {
         return listOf(ApkInfo(assetsApkFile, androidApkPackage))
-    }
-}
-
-class SyncExecutorService: ExecutorService {
-    override fun execute(command: Runnable) {
-        TODO("Not yet implemented")
-    }
-
-    override fun shutdown() {
-        TODO("Not yet implemented")
-    }
-
-    override fun shutdownNow(): MutableList<Runnable> {
-        TODO("Not yet implemented")
-    }
-
-    override fun isShutdown(): Boolean {
-        return false
-    }
-
-    override fun isTerminated(): Boolean {
-        return false
-    }
-
-    override fun awaitTermination(timeout: Long, unit: TimeUnit): Boolean {
-        return true
-    }
-
-    override fun <T : Any?> submit(task: Callable<T>): Future<T> {
-        val result = task.call()
-        return CompletedFuture(result, null)
-    }
-
-    override fun <T : Any?> submit(task: Runnable, result: T): Future<T> {
-        task.run()
-        return CompletedFuture(result, null)
-    }
-
-    override fun submit(task: Runnable): Future<*> {
-        task.run()
-        return CompletedFuture(null, null)
-    }
-
-    override fun <T : Any?> invokeAll(tasks: MutableCollection<out Callable<T>>): MutableList<Future<T>> {
-        TODO("Not yet implemented")
-    }
-
-    override fun <T : Any?> invokeAll(
-        tasks: MutableCollection<out Callable<T>>,
-        timeout: Long,
-        unit: TimeUnit
-    ): MutableList<Future<T>> {
-        TODO("Not yet implemented")
-    }
-
-    override fun <T : Any?> invokeAny(tasks: MutableCollection<out Callable<T>>): T {
-        TODO("Not yet implemented")
-    }
-
-    override fun <T : Any?> invokeAny(tasks: MutableCollection<out Callable<T>>, timeout: Long, unit: TimeUnit): T {
-        TODO("Not yet implemented")
-    }
-
-}
-
-open class CompletedFuture<T>(private val v: T, private val re: Throwable?) : Future<T> {
-    override fun cancel(mayInterruptIfRunning: Boolean): Boolean {
-        return false
-    }
-
-    override fun isCancelled(): Boolean {
-        return false
-    }
-
-    override fun isDone(): Boolean {
-        return true
-    }
-
-    @Throws(ExecutionException::class)
-    override fun get(): T {
-        return if (re != null) {
-            throw ExecutionException(re)
-        } else {
-            v
-        }
-    }
-
-    @Throws(ExecutionException::class)
-    override fun get(timeout: Long, unit: TimeUnit): T {
-        return this.get()
     }
 }
 
