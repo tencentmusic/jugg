7d4e46d5910071f94c8138d14bfa64d460efa365
sickworm <ch.operation@gmail.com>
2022-03-13 11:12:06 +0800

    [test] split test code from JuggMock

--- src/test/java/com/sickworm/intellij/jugg/manager/BasicJuggMock.kt
+++ src/test/java/com/sickworm/intellij/jugg/manager/BasicJuggMock.kt
@@ -48,7 +48,36 @@ class BasicJuggMock {
     lateinit var juggDeployerHelper: JuggDeployerHelper
     lateinit var deployDataManager: DeployDataManager
 
+    // TODO use adb command
     val device = DeviceImpl(null, "192.168.31.243:33105", IDevice.DeviceState.ONLINE)
+
+    companion object {
+        private var hasInitOnce: Boolean = false
+    }
+
+    fun initEnv() {
+        if (!hasInitOnce) {
+            hasInitOnce = true
+            MockitoFixer.tryFix()
+            AndroidDebugBridge.init(true)
+        }
+        BuildDemoApkTest().buildApkIfNeeded()
+    }
+
+    fun resetAllState() {
+        clearBuild()
+        renewComponents()
+        renewManager()
+        markAsReadyToDeploy()
+    }
+
+    /**
+     * Just simply mark changes as deployed. Use this we don't need an android device to run tests.
+     */
+    fun dryDeploy() {
+        val deployData = deployDataManager.getDeployData()
+        deployDataManager.commit(deployData)
+    }
 
     private fun renewComponents() {
         val application = MockApplication {}
@@ -118,93 +147,5 @@ class BasicJuggMock {
         assertEquals(1, compileContextManager.compileContext.parsedApks.size)
         assertTrue(::fileChangeEventSender.isInitialized)
         verify(deviceStatusListener, times(1)).updateStatus(state)
-    }
-
-    private val testSourceDirectory = "app/src/main/java/${androidApkPackage.replace('.', '/')}"
-
-    fun changeFileAndNotify(vararg fileNamePairs: Pair<String, String>, directory: String = testSourceDirectory) {
-        val pairs = fileNamePairs.map { (sourceFileName, destFileName) ->
-            val sourceFile = File(assetsAndroidModifySourceDir, "$directory/$sourceFileName")
-            val destFile = File(assetsAndroidDir, "$directory/$destFileName")
-            sourceFile to destFile
-        }
-        val revertFileMark = pairs.map { (_, destFile) ->
-            destFile to destFile.exists()
-        }
-        fileChangeEventSender.copyAndNotifyFileChanges(pairs)
-
-        // revert
-        revertFileMark.forEach { (destFile, isExist) ->
-            revertFile(destFile.name, isAdd = !isExist, directory = directory)
-        }
-    }
-
-    private fun revertFile(originFile: String, isAdd: Boolean = false, directory: String) {
-        val sourceFile = File(assetsAndroidModifySourceDir, "$directory/$originFile")
-        val destFile = File(assetsAndroidDir, "$directory/$originFile")
-        if (isAdd) {
-            destFile.delete()
-            return
-        }
-        sourceFile.copyTo(destFile, overwrite = true)
-    }
-
-    fun checkCompileResult(
-        vararg fileNames: String,
-        filePackageName: String = androidApkPackage,
-        newClassesSize: Int = 0,
-        hotFixModifiedClassesSize: Int = 0,
-        hotReloadModifiedClassesSize: Int = 0,
-        overlaysSize: Int = 0,
-    ) {
-        fileNames.forEach { fileName ->
-            val relativePath = filePackageName.replace('.', '/')
-            val className = File(fileName).nameWithoutExtension + ".class"
-            val classPathFile = File(compileContextManager.compileContext.classPathDir, "$relativePath/$className")
-            assertTrue(classPathFile.exists(), ".class file not exists, path: $classPathFile")
-            assertTrue(classPathFile.length() > 0)
-
-            val dexName = File(fileName).nameWithoutExtension + ".dex"
-            val dexFile = File(compileContextManager.stagingDir, "classes/$relativePath/$dexName")
-            assertTrue(dexFile.exists(), ".dex file not exists, path: $classPathFile")
-            assertTrue(dexFile.length() > 0)
-        }
-
-        assertEquals(0, deployDataManager.getUncompiledFiles().size)
-        val deployData = deployDataManager.getDeployData()
-        assertEquals(1, deployData.apks.size)
-        assertEquals(newClassesSize, deployData.newClasses.size)
-        assertEquals(hotFixModifiedClassesSize, deployData.hotFixModifiedClasses.size)
-        assertEquals(hotReloadModifiedClassesSize, deployData.hotReloadModifiedClasses.size)
-        assertEquals(overlaysSize, deployData.overlays.size)
-    }
-
-
-    companion object {
-        private var hasInitOnce: Boolean = false
-    }
-
-    fun initEnv() {
-        if (!hasInitOnce) {
-            hasInitOnce = true
-            MockitoFixer.tryFix()
-            AndroidDebugBridge.init(true)
-        }
-        BuildDemoApkTest().buildApkIfNeeded()
-    }
-
-    fun resetAllState() {
-        clearBuild()
-        renewComponents()
-        renewManager()
-        markAsReadyToDeploy()
-    }
-
-    /**
-     * Just simply mark changes as deployed. Use this we don't need an android device to run tests.
-     */
-    fun dryDeploy() {
-        val deployData = deployDataManager.getDeployData()
-        deployDataManager.commit(deployData)
     }
 }
