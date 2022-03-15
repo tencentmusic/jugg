8a6e70003a6b83b6e8ab75b41bde09eba3a44f9a
sickworm <ch.operation@gmail.com>
2022-03-15 11:31:57 +0800

    [test] dummyPropertiesComponent supports setValue

--- src/test/java/com/sickworm/intellij/jugg/mock/DummyPropertiesComponent.kt
+++ src/test/java/com/sickworm/intellij/jugg/mock/DummyPropertiesComponent.kt
@@ -3,38 +3,53 @@ package com.sickworm.intellij.jugg.mock
 import com.intellij.ide.util.PropertiesComponent
 
 class DummyPropertiesComponent: PropertiesComponent() {
-    override fun unsetValue(name: String) {
 
+    private val map: MutableMap<String, String?> = mutableMapOf()
+
+    override fun unsetValue(name: String) {
+        map.remove(name)
     }
 
     override fun isValueSet(name: String): Boolean {
-        return false
+        return map.containsKey(name)
     }
 
     override fun getValue(name: String): String? {
-        return null
+        return map[name]
     }
 
     override fun setValue(name: String, value: String?) {
+        map[name] = value
     }
 
     override fun setValue(name: String, value: String?, defaultValue: String?) {
+        if (value == null || value == defaultValue) {
+            unsetValue(name)
+        }
+        map[name] = value
     }
 
     override fun setValue(name: String, value: Float, defaultValue: Float) {
+        map[name] = value.toString()
     }
 
     override fun setValue(name: String, value: Int, defaultValue: Int) {
+        map[name] = value.toString()
     }
 
     override fun setValue(name: String, value: Boolean, defaultValue: Boolean) {
+        if (value == defaultValue) {
+            unsetValue(name)
+        }
+        map[name] = value.toString()
     }
 
     override fun getValues(name: String): Array<String>? {
-        return null
+        throw IllegalArgumentException("not supported")
     }
 
     override fun setValues(name: String, values: Array<out String>?) {
+        throw IllegalArgumentException("not supported")
     }
 
 }
