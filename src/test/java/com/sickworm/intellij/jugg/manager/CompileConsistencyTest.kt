51f757f6e25003c1d5d445e36ddb7011dc837966
sickworm <ch.operation@gmail.com>
2022-04-01 20:47:18 +0800

    [test] supports overlays binary comparison

--- src/test/java/com/sickworm/intellij/jugg/manager/CompileConsistencyTest.kt
+++ src/test/java/com/sickworm/intellij/jugg/manager/CompileConsistencyTest.kt
@@ -1,9 +1,13 @@
 package com.sickworm.intellij.jugg.manager
 
+import com.android.tools.deployer.DeployItem
+import com.android.tools.deployer.JuggDeployData
 import com.sickworm.intellij.jugg.compiler.CompileFile
+import com.sickworm.intellij.jugg.compiler.CompileOutput
 import com.sickworm.intellij.jugg.compiler.isResourceValueFile
 import com.sickworm.intellij.jugg.ide.JuggSettings
 import com.sickworm.intellij.jugg.mock.assetsAndroidDir
+import com.sickworm.intellij.jugg.project.ChangedFile
 import org.junit.AfterClass
 import org.junit.Assert
 import org.junit.BeforeClass
@@ -11,6 +15,7 @@ import org.junit.Test
 import java.io.File
 import java.nio.file.*
 import java.nio.file.attribute.BasicFileAttributes
+import java.util.zip.ZipFile
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
@@ -44,8 +49,20 @@ class CompileConsistencyTest {
     @Test
     fun testConsistency() {
 
-        val fileList = mutableListOf<File>()
         val rootDir = assetsAndroidDir
+        val fileList = getCheckFiles(rootDir)
+        println("${fileList.size} files to be check (including not compilable files)")
+
+        for (file in fileList) {
+            println("checking ${file.relativeTo(rootDir)}...")
+            checkFileCompileConsistency(file)
+        }
+
+        System.err.println(failedBinaryCheckList.toList())
+    }
+
+    private fun getCheckFiles(rootDir: File): List<File> {
+        val fileList = mutableListOf<File>()
         Files.walkFileTree(rootDir.toPath(), object: SimpleFileVisitor<Path>() {
             override fun preVisitDirectory(dir: Path, attrs: BasicFileAttributes): FileVisitResult {
                 val fileName = dir.fileName.toString()
@@ -71,13 +88,7 @@ class CompileConsistencyTest {
                 return FileVisitResult.CONTINUE
             }
         })
-
-        println("${fileList.size} files to be check (including not compilable files)")
-
-        for (file in fileList) {
-            println("checking ${file.relativeTo(rootDir)}...")
-            checkFileCompileConsistency(file)
-        }
+        return fileList
     }
 
     private fun checkFileCompileConsistency(file: File) {
@@ -93,7 +104,35 @@ class CompileConsistencyTest {
 
         jugg.compileChangedFiles()
 
+        if (firstTimeDeployOverlays) {
+            if (uncompiledFile.type == CompileFile.Type.Resource || uncompiledFile.type == CompileFile.Type.Asset) {
+                println("FirstTime deploy overlays needs full deployment, dry deploy and recompile again.")
+                firstTimeDeployOverlays = false
+                jugg.dryDeploy()
+                checkFileCompileConsistency(file)
+                return
+            }
+        }
+
         val deployData = jugg.deployDataManager.getDeployData()
+        checkDeployData(uncompiledFile, deployData)
+        val apk = jugg.deployTargetManager.getApks().first().file
+        val deployItems = listOf(
+            deployData.hotFixModifiedClasses,
+            deployData.hotReloadModifiedClasses,
+            deployData.newClasses,
+            deployData.overlays
+        ).flatten()
+        deployItems.forEach {
+            checkCompileBinary(it, apk)
+        }
+
+        jugg.dryDeploy()
+
+        println("check consistency passed")
+    }
+
+    private fun checkDeployData(uncompiledFile: ChangedFile, deployData: JuggDeployData) {
         val errorMessage = deployData.toString()
 
         when (uncompiledFile.type) {
@@ -104,13 +143,6 @@ class CompileConsistencyTest {
                 assertEquals(0, deployData.overlays.size, errorMessage)
             }
             CompileFile.Type.Resource, CompileFile.Type.Asset -> {
-                if (firstTimeDeployOverlays) {
-                    println("FirstTime deploy overlays needs full deployment, dry deploy and recompile again.")
-                    firstTimeDeployOverlays = false
-                    jugg.dryDeploy()
-                    checkFileCompileConsistency(file)
-                    return
-                }
                 assertEquals(0, deployData.newClasses.size, errorMessage)
                 assertEquals(0, deployData.hotFixModifiedClasses.size, errorMessage)
                 if (uncompiledFile.type == CompileFile.Type.Resource) {
@@ -131,9 +163,68 @@ class CompileConsistencyTest {
                 Assert.fail("Unexpected compile file type ${uncompiledFile.type} for file ${uncompiledFile.file}")
             }
         }
-
-        jugg.dryDeploy()
-
-        println("check consistency passed")
     }
+
+    private fun checkCompileBinary(deployItem: DeployItem, apk: File) {
+        when (deployItem.type) {
+            CompileOutput.Type.Dex -> {
+                val bytes = getClassBytesFromApk(deployItem, apk)
+                compareBinary(deployItem, bytes, deployItem.content)
+            }
+            CompileOutput.Type.Overlay -> {
+                val bytes = getOverlayBytesFromApk(deployItem, apk)
+                compareBinary(deployItem, bytes, deployItem.content)
+            }
+            else -> {
+                Assert.fail("Unexpected deploy file type ${deployItem.type}")
+            }
+        }
+    }
+
+    private fun getClassBytesFromApk(deployItem: DeployItem, apk: File): ByteArray {
+        // TODO
+        return deployItem.content
+    }
+
+    private fun getOverlayBytesFromApk(deployItem: DeployItem, apk: File): ByteArray? {
+        val path = deployItem.name
+        val zipFile = ZipFile(apk)
+        val entry = zipFile.getEntry(path) ?: return null
+        val inputStream = zipFile.getInputStream(entry)
+        return inputStream.readAllBytes()
+    }
+
+    private val failedBinaryCheckList = mutableListOf<String>()
+
+    private fun compareBinary(deployItem: DeployItem, except: ByteArray?, actual: ByteArray) {
+        // Currently not supported case
+        // TODO support ignoreBinaryCheckList
+        if (deployItem.name in ignoreBinaryCheckList) {
+            return
+        }
+
+        if (except == null) {
+            // not exists in apk, it's ok
+            return
+        }
+
+        if (!except.contentEquals(actual)) {
+            val message = """
+                file: ${deployItem.name}
+                except size: ${except.size}, actual size: ${actual.size}
+                except content:
+                ${String(except)}
+                actual content:
+                ${String(actual)}
+            """.trimIndent()
+            Assert.fail(message)
+            failedBinaryCheckList.add(deployItem.name)
+        }
+    }
+
+    private val ignoreBinaryCheckList = listOf(
+        "resources.arsc",
+        "res/drawable-v24/\$ic_launcher_foreground__0.xml",
+        "res/mipmap-xxxhdpi-v4/ic_launcher.png",
+    )
 }
