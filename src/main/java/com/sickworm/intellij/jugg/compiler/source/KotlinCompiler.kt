1c53f7b589aa7eb8ae39c6d0613d047877ecd263
sickworm <ch.operation@gmail.com>
2022-04-25 01:35:40 +0800

    [bugfix] fix "Check your module classpath for missing or conflicting dependencies" and "supertypes of the following classes cannot be resolved" when has multiple sourceDirs

--- src/main/java/com/sickworm/intellij/jugg/compiler/source/KotlinCompiler.kt
+++ src/main/java/com/sickworm/intellij/jugg/compiler/source/KotlinCompiler.kt
@@ -69,7 +69,7 @@ class KotlinCompiler(context: ICompileContext): BaseCompiler(context) {
             "-Xallow-no-source-files",
             "-Xreport-output-files",
             // resolve "class is not abstract and does not implement abstract member"
-            "-Xjava-source-roots=${module.sourceDirs.joinToString(", ")}",
+            "-Xjava-source-roots=${module.sourceDirs.joinToString(",")}",
             // we have to set output dir to kotlin compiled class path to resolve
             // 'xxx' is a public API property declared in different module
             "-d", kotlinClassPath.absolutePath,
