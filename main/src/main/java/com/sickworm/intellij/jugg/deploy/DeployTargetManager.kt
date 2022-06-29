57197773eebe264ad3535a540ac4db496ba62766
sickworm <ch.operation@gmail.com>
2022-06-29 14:19:11 +0800

    [bugfix] fix runFullBuildAndLaunch won't waiting build end

--- main/src/main/java/com/sickworm/intellij/jugg/deploy/DeployTargetManager.kt
+++ main/src/main/java/com/sickworm/intellij/jugg/deploy/DeployTargetManager.kt
@@ -4,17 +4,21 @@ import com.android.ddmlib.*
 import com.android.tools.idea.run.AndroidRunConfiguration
 import com.android.tools.idea.run.ApkInfo
 import com.android.tools.idea.run.ApkProvider
+import com.intellij.execution.ExecutionException
 import com.intellij.execution.ProgramRunnerUtil
 import com.intellij.execution.RunManager
 import com.intellij.execution.RunnerAndConfigurationSettings
 import com.intellij.execution.executors.DefaultRunExecutor
-import com.intellij.openapi.application.ApplicationManager
+import com.intellij.execution.runners.ExecutionEnvironmentBuilder
 import com.intellij.openapi.project.Project
 import com.sickworm.intellij.jugg.deploy.run.AsDeployerCompat
+import com.sickworm.intellij.jugg.logger.JuggLogger
 import com.sickworm.intellij.jugg.project.JuggException
 import com.sickworm.intellij.jugg.project.JuggInternalException
-import com.sickworm.intellij.jugg.logger.JuggLogger
 
+/**
+ * Manage device, run config, built apk.
+ */
 class DeployTargetManager(
     private val project: Project,
 ): IDeployTargetManager {
@@ -22,8 +26,24 @@ class DeployTargetManager(
 
     override fun runFullBuildAndLaunch() {
         val (runConfigAndSettings, _) = getRunConfig()
-        ApplicationManager.getApplication().invokeAndWait {
-            ProgramRunnerUtil.executeConfiguration(runConfigAndSettings, DefaultRunExecutor.getRunExecutorInstance())
+        val executor = DefaultRunExecutor.getRunExecutorInstance()
+        ProgramRunnerUtil.executeConfiguration(runConfigAndSettings, DefaultRunExecutor.getRunExecutorInstance())
+        val builder: ExecutionEnvironmentBuilder = try {
+            ExecutionEnvironmentBuilder.create(executor, runConfigAndSettings)
+        } catch (e: ExecutionException) {
+            logger.error("runFullBuildAndLaunch create builder failed", e)
+            return
+        }
+        val environment = builder.contentToReuse(null).dataContext(null).activeTarget().build()
+
+        val lock = Object()
+        ProgramRunnerUtil.executeConfigurationAsync(environment, true, true) {
+            synchronized(lock) {
+                lock.notify()
+            }
+        }
+        synchronized(lock) {
+            lock.wait()
         }
     }
 
