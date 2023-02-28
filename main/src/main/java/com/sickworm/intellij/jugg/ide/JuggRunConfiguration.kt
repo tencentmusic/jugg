1babda409fc23f5ca605b36811ac75097388c7b9
sickworm <ch.operation@gmail.com>
2023-02-28 09:44:41 +0800

    [feature] [WIP] add Jugg run configuration, remote compile success, but FetchOutputCommand still success when no client found.

--- main/src/main/java/com/sickworm/intellij/jugg/ide/JuggRunConfiguration.kt
+++ main/src/main/java/com/sickworm/intellij/jugg/ide/JuggRunConfiguration.kt
@@ -1,16 +1,29 @@
 package com.sickworm.intellij.jugg.ide
 
+import com.google.gson.Gson
+import com.intellij.execution.DefaultExecutionResult
+import com.intellij.execution.ExecutionResult
 import com.intellij.execution.Executor
 import com.intellij.execution.configurations.*
+import com.intellij.execution.filters.TextConsoleBuilderFactory
 import com.intellij.execution.process.*
 import com.intellij.execution.runners.ExecutionEnvironment
+import com.intellij.execution.runners.ProgramRunner
 import com.intellij.icons.AllIcons
 import com.intellij.openapi.options.SettingsEditor
 import com.intellij.openapi.progress.*
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Key
+import com.sickworm.intellij.jugg.remote.RemoteClient
+import com.sickworm.intellij.jugg.remote.RemoteCompileClientInfo
+import org.jetbrains.kotlin.utils.addToStdlib.measureTimeMillisWithResult
+import java.io.File
+import java.io.OutputStream
 import javax.swing.JComponent
 import javax.swing.JTextField
+import kotlin.system.measureTimeMillis
+import kotlin.time.measureTime
+
 
 class JuggRunConfiguration(
     project: Project,
@@ -27,7 +40,7 @@ class JuggRunConfiguration(
     }
 
     override fun getState(executor: Executor, environment: ExecutionEnvironment): RunProfileState {
-        return JuggRunProfileState(project, environment)
+        return JuggRunProfileState(project)
     }
 }
 
@@ -67,37 +80,75 @@ class JuggSettingsEditor : SettingsEditor<JuggRunConfiguration>() {
 
 }
 
-class JuggRunProfileState(val project: Project, environment: ExecutionEnvironment) : CommandLineState(environment) {
+class JuggRunProfileState(val project: Project) : RunProfileState {
 
-    override fun startProcess(): ProcessHandler {
-        // ; sleep 1;ls /Users/wormchen; sleep 1;ls /Users/wormchen; sleep 1;echo "done!"
-        val command = "ls /Users/wormchen".split(" ")
-        val cmd = GeneralCommandLine(command)
-        val processHandler = ColoredProcessHandler(cmd)
-        ProcessTerminatedListener.attach(processHandler)
+    val logKey = Key.create<String>("Jugg Terminal")
 
-        val progressManager = ProgressManager.getInstance()
-        val task = object : Task.Backgroundable(project, "Running Jugg") {
-            override fun run(indicator: ProgressIndicator) {
-                indicator.text = "Running command"
-                indicator.isIndeterminate = true
-                processHandler.addProcessListener(object : ProcessAdapter() {
-                    override fun onTextAvailable(event: ProcessEvent, outputType: Key<*>) {
-                        indicator.text = "Running command: $event"
-                    }
-
-                    override fun processTerminated(event: ProcessEvent) {
-                    }
-                })
-
-                processHandler.startNotify()
-                val exitCode = processHandler.waitFor()
-                println("finish: $exitCode")
+    override fun execute(executor: Executor?, runner: ProgramRunner<*>): ExecutionResult {
+        // TODO support cancel
+        val remoteClient = RemoteClient(project, project)
+        val consoleView = TextConsoleBuilderFactory.getInstance().createBuilder(project).console
+        val processHandler = SimpleProcessHandler()
+        remoteClient.terminalOutputListener = object : RemoteClient.TerminalOutputListener {
+            override fun onOutput(line: String) {
+                processHandler.notifyTextAvailable(line + "\n", logKey)
             }
         }
 
-        val indicator = EmptyProgressIndicator()
-        progressManager.runProcessWithProgressAsynchronously(task, indicator)
-        return processHandler
+        @Suppress("DialogTitleCapitalization")
+        val task = object : Task.Backgroundable(project, "Running Jugg") {
+            override fun run(indicator: ProgressIndicator) {
+                processHandler.notifyTextAvailable("\n\n[Jugg] Compile finished.\n", logKey)
+                indicator.text = "Jugg compiling..."
+                indicator.isIndeterminate = true
+
+                val (costTime, isSuccess) = measureTimeMillisWithResult(::doRun)
+
+                indicator.stop()
+                val result = if (isSuccess) "SUCCESS" else "FAILED"
+                processHandler.notifyTextAvailable("\n\n[Jugg] BUILD $result in ${costTime / 1000}s.\n", logKey)
+            }
+
+            private fun doRun(): Boolean {
+                val homeDir = System.getProperty("user.home")
+                val clientInfoFile = File("$homeDir/Downloads/remote_compile_client_info.json")
+                val clientInfo = Gson().fromJson(clientInfoFile.readText(), RemoteCompileClientInfo::class.java)
+
+                remoteClient.login(clientInfo)
+                val remoteCompileResult = remoteClient.compileAndFetchResult()
+                return remoteCompileResult.isSuccess
+            }
+        }
+
+        consoleView.attachToProcess(processHandler)
+
+        ProgressManager.getInstance().run(task)
+        return DefaultExecutionResult(consoleView, processHandler)
+    }
+
+    private class SimpleProcessHandler : ProcessHandler(), AnsiEscapeDecoder.ColoredTextAcceptor {
+        private val myAnsiEscapeDecoder = AnsiEscapeDecoder()
+
+        override fun destroyProcessImpl() = Unit
+
+        override fun detachProcessImpl() = Unit
+
+        override fun detachIsDefault() = true
+
+        override fun waitFor() = true
+
+        override fun waitFor(timeoutInMilliseconds: Long) = true
+
+        override fun getProcessInput(): OutputStream? {
+            return null
+        }
+
+        override fun notifyTextAvailable(text: String, outputType: Key<*>) {
+            myAnsiEscapeDecoder.escapeText(text, outputType, this)
+        }
+
+        override fun coloredTextAvailable(text: String, attributes: Key<*>) {
+            super.notifyTextAvailable(text, attributes)
+        }
     }
 }
--- main/src/main/java/com/sickworm/intellij/jugg/remote/RemoteClient.kt
+++ main/src/main/java/com/sickworm/intellij/jugg/remote/RemoteClient.kt
@@ -9,6 +9,7 @@ import com.sickworm.intellij.jugg.logger.JuggLogger
 import com.sickworm.intellij.jugg.project.JuggException
 import com.sickworm.intellij.jugg.project.JuggInternalException
 import java.io.File
+import java.io.PipedOutputStream
 import java.io.PrintStream
 
 class RemoteClient(project: Project, parent: Disposable) : IRemoteClient, Disposable {
@@ -18,13 +19,15 @@ class RemoteClient(project: Project, parent: Disposable) : IRemoteClient, Dispos
     private var clientInfo: RemoteCompileClientInfo? = null
     private val logger = JuggLogger.getInstance(project, "RemoteClient")
 
+    var terminalOutputListener: TerminalOutputListener? = null
+
     init {
         Disposer.register(parent, this)
     }
 
     override fun login(clientInfo: RemoteCompileClientInfo) {
         if ((this.clientInfo == clientInfo) && (session?.isConnected == true) && channel != null) {
-            logger.info("${clientInfo.ip} already login")
+            printToStreamInfo("${clientInfo.ip} already login")
             return
         }
 
@@ -32,7 +35,7 @@ class RemoteClient(project: Project, parent: Disposable) : IRemoteClient, Dispos
 
         try {
             val jsch = JSch()
-            JSch.setLogger(JschLogger())
+            JSch.setLogger(JschLogger { terminalOutputListener })
             val session = jsch.getSession(clientInfo.user, clientInfo.ip, clientInfo.port)
             if (clientInfo.httpProxyIp != null && clientInfo.httpProxyPort != null) {
                 session.setProxy(ProxyHTTP(clientInfo.httpProxyIp, clientInfo.httpProxyPort))
@@ -47,7 +50,7 @@ class RemoteClient(project: Project, parent: Disposable) : IRemoteClient, Dispos
             this.channel = channel
             this.clientInfo = clientInfo
         } catch (e: JSchException) {
-            logger.warn("RemoteClient login failed", e)
+            printToStreamError("RemoteClient login failed", e)
             throw JuggException.loginToRemoteFailed()
         }
     }
@@ -62,14 +65,14 @@ class RemoteClient(project: Project, parent: Disposable) : IRemoteClient, Dispos
         val syncFileCommand = SyncFileCommand(clientInfo.localProjectPath, clientInfo.remoteProjectPath)
         val syncFileResult = invoke(channel, syncFileCommand)
         if (syncFileResult != 0) {
-            logger.warn("Sync file from local failed, please check your iFt client is opened.")
+            printToStreamError("Sync file from local failed, please check your iFt client is opened.")
             return RemoteCompileResult.failed()
         }
 
         val compileProjectCommand = CompileProjectCommand(clientInfo.remoteProjectPath)
         val compileProjectResult = invoke(channel, compileProjectCommand)
         if (compileProjectResult != 0) {
-            logger.warn("Compile project failed, please check your iFt client is opened.")
+            printToStreamError("Compile project failed, please check your iFt client is opened.")
             return RemoteCompileResult.failed()
         }
 
@@ -77,7 +80,7 @@ class RemoteClient(project: Project, parent: Disposable) : IRemoteClient, Dispos
         val fetchOutputCommand = FetchOutputCommand(clientInfo.remoteToLocalIftConfigName)
         val fetchOutputResult = invoke(channel, fetchOutputCommand)
         if (fetchOutputResult != 0) {
-            logger.warn("Fetch output failed, please check your iFt client is opened.")
+            printToStreamError("Fetch output failed, please check your iFt client is opened.")
             return RemoteCompileResult.failed()
         }
 
@@ -98,14 +101,14 @@ class RemoteClient(project: Project, parent: Disposable) : IRemoteClient, Dispos
         )
         val fetchClasspathResult = invoke(channel, fetchClasspathCommand)
         if (fetchClasspathResult != 0) {
-            logger.warn("Fetch classpath failed, please check your iFt client is opened.")
+            printToStreamError("Fetch classpath failed, please check your iFt client is opened.")
             return false
         }
         return true
     }
 
     private fun invoke(channel: Channel, command: ISshCommand): Int {
-        println("${command::class.simpleName} exec start")
+        printToStream("${command::class.simpleName} exec start")
 
         val commander = PrintStream(channel.outputStream, true)
         commander.println(command.command)
@@ -117,7 +120,7 @@ class RemoteClient(project: Project, parent: Disposable) : IRemoteClient, Dispos
             val line = reader.readLine()
             if (line != null) {
                 if (line.isNotEmpty()) {
-                    println(line)
+                    printToStream(line)
                 }
                 val output = command.getInput(line)
                 if (output != null) {
@@ -132,15 +135,33 @@ class RemoteClient(project: Project, parent: Disposable) : IRemoteClient, Dispos
             }
 
             if (channel.isClosed) {
-                println("exit-status: " + channel.exitStatus)
+                printToStream("exit-status: " + channel.exitStatus)
                 result = RESULT_CHANNEL_CLOSED
                 break
             }
         }
 
-        println("${command::class.simpleName} exec finished with result: $result")
+        printToStream("${command::class.simpleName} exec finished with result: $result")
         return result
     }
+
+    private fun printToStream(line: String) {
+        terminalOutputListener?.onOutput(line)
+    }
+
+    private fun printToStreamInfo(line: String) {
+        logger.info(line)
+        terminalOutputListener?.onOutput(line)
+    }
+
+    private fun printToStreamError(line: String, e: Exception? = null) {
+        logger.warn(line, e)
+        terminalOutputListener?.onOutput(line)
+        e?.let {
+            terminalOutputListener?.onOutput(e.toString())
+        }
+    }
+
 
     companion object {
         const val RESULT_CHANNEL_CLOSED = -1001
@@ -152,16 +173,24 @@ class RemoteClient(project: Project, parent: Disposable) : IRemoteClient, Dispos
         session = null
         channel = null
     }
+
+    interface TerminalOutputListener {
+        fun onOutput(line: String)
+    }
 }
 
 
-class JschLogger : Logger {
+class JschLogger(
+    private val terminalOutputListener: () -> RemoteClient.TerminalOutputListener?,
+) : Logger {
+
     override fun isEnabled(level: Int): Boolean {
         return true
     }
 
     override fun log(level: Int, message: String) {
-        println(name(level) + ": " + message)
+        val levelMessage = name(level) + ": " + message
+        terminalOutputListener.invoke()?.onOutput(levelMessage)
     }
 
     private fun name(level: Int): String {
