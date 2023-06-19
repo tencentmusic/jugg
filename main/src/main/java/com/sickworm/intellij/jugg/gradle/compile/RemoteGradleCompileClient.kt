61053a99028998113fb24d3fee500ad20d177e89
sickworm <ch.operation@gmail.com>
2023-06-19 10:22:31 +0800

    [bugfix] fix compile log output twice

--- main/src/main/java/com/sickworm/intellij/jugg/gradle/compile/RemoteGradleCompileClient.kt
+++ main/src/main/java/com/sickworm/intellij/jugg/gradle/compile/RemoteGradleCompileClient.kt
@@ -192,15 +192,10 @@ class RemoteGradleCompileClient(
 
     private fun printToStreamInfo(line: String) {
         logger.info(line)
-        terminalOutputListener.onOutput(line)
     }
 
     private fun printToStreamError(line: String, e: Exception? = null) {
         logger.warn(line, e)
-        terminalOutputListener.onOutputErr(line)
-        e?.let {
-            terminalOutputListener.onOutputErr(e.toString())
-        }
     }
 
     private fun printToStreamErrorIfCanceled(line: String, e: Exception? = null) {
