08ecf9831b72b01f0fc410deaf639d53d91d4474
sickworm <ch.operation@gmail.com>
2023-08-03 20:26:35 +0800

    [feature] add data reporter for Jugg

---
+++ main/src/test/java/com/sickworm/intellij/jugg/JuggReporterTest.kt
@@ -0,0 +1,31 @@
+package com.sickworm.intellij.jugg
+
+import com.sickworm.intellij.jugg.logger.JuggLogger
+import com.sickworm.intellij.jugg.logger.JuggReporter
+import com.sickworm.intellij.jugg.mock.JuggMockProject
+import com.sickworm.intellij.jugg.mock.buildDir
+import com.sickworm.intellij.jugg.mock.logger
+import com.sickworm.intellij.jugg.mock.projectInfo
+import kotlinx.coroutines.joinAll
+import kotlinx.coroutines.runBlocking
+import org.junit.Test
+
+class JuggReporterTest {
+
+    @Test
+    fun testReport() {
+        val project = JuggMockProject(projectInfo.projectRoot)
+        JuggLogger.register(project, buildDir)
+        JuggLogger.listenProjectLog(project, logger)
+        JuggReporter.init(project)
+        runBlocking {
+            val job1 = JuggReporter.report {
+                isSuccess = true
+            }
+            val job2 = JuggReporter.report {
+                isSuccess = false
+            }
+            listOf(job1, job2).joinAll()
+        }
+    }
+}
