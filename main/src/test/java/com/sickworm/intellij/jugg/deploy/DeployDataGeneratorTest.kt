a2064ee7f4f9ca7e71f7d6ae83efd25156f22e62
sickworm <ch.operation@gmail.com>
2023-08-06 23:45:09 +0800

    [test] add DeployDataGeneratorTest

---
+++ main/src/test/java/com/sickworm/intellij/jugg/deploy/DeployDataGeneratorTest.kt
@@ -0,0 +1,23 @@
+package com.sickworm.intellij.jugg.deploy
+
+import com.sickworm.intellij.jugg.compiler.CompileOutput
+import com.sickworm.intellij.jugg.deploy.run.DeployItem
+import com.sickworm.intellij.jugg.mock.logger
+import com.sickworm.intellij.jugg.mock.projectInfo
+import org.junit.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+class DeployDataGeneratorTest {
+
+    @Test
+    fun testOverlayContents() {
+        val generator = DeployDataGenerator(logger)
+        generator.init(projectInfo.apkInfos)
+        val overlayDeployItem = DeployItem("test_overlay", CompileOutput.Type.Overlay, 0, byteArrayOf())
+        val data = generator.buildDeployData(listOf(overlayDeployItem))
+        assertEquals(475, data.overlays.size)
+        assertTrue(data.isFullOverlays)
+        logger.debug(data.toString())
+    }
+}
