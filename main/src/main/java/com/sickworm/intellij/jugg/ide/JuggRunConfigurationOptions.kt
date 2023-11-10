85259f80ac11af7d27d77997916f65be1a08e583
sickworm <ch.operation@gmail.com>
2023-11-10 11:32:14 +0800

    [optimize] remove redundant /

--- main/src/main/java/com/sickworm/intellij/jugg/ide/JuggRunConfigurationOptions.kt
+++ main/src/main/java/com/sickworm/intellij/jugg/ide/JuggRunConfigurationOptions.kt
@@ -60,10 +60,10 @@ data class JuggGradleCompileOptions(
     val localSyncIftPath get() = "$localToRemoteIftConfigName/$projectSyncRootRelativePath"
 
     /** remote project sync path, used for syncing files to remote by iFt, and fetching classpath */
-    val remoteSyncRootPath get() = "/$remoteBasePath/$localToRemoteIftConfigName/$projectSyncRootRelativePath"
+    val remoteSyncRootPath get() = "$remoteBasePath/$localToRemoteIftConfigName/$projectSyncRootRelativePath"
 
     /** remote project root path, used for compilation */
-    val remoteProjectPath get() = "/$remoteBasePath/$localToRemoteIftConfigName/$projectSyncRelativePath" // use ~/ will make path replacement don't work
+    val remoteProjectPath get() = "$remoteBasePath/$localToRemoteIftConfigName/$projectSyncRelativePath" // use ~/ will make path replacement don't work
 
     /** remote iFt path, used for fetching apk output to local */
     val remoteToLocalProjectIftPath get() = "$remoteToLocalIftConfigName/$projectSyncRelativePath"
