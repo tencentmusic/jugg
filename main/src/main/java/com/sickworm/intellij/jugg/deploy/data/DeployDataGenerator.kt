cd5e8962f50ffc082f92bda7e3cb5d662e8cb510
sickworm <ch.operation@gmail.com>
2023-12-21 00:39:29 +0800

    [bugfix] fix NoSuchMethodError by effected class not found, after inner static class method / companion class method changed

--- main/src/main/java/com/sickworm/intellij/jugg/deploy/data/DeployDataGenerator.kt
+++ main/src/main/java/com/sickworm/intellij/jugg/deploy/data/DeployDataGenerator.kt
@@ -73,14 +73,10 @@ class DeployDataGenerator(
 
             // we don't care about abstract, because it won't affect class bytecode.
             // ignore abstract can stop recompile when redex interface class default method (which will make methods be not abstract)
-            if (className.isInnerClass && !result.isCanHotReload) {
-                logger.debug("class $className is inner class, no need to find effected classes")
-            } else {
-                changedMethodRef.addAll(result.effectMethods)
-                changedFieldRef.addAll(result.deletedFields)
-                if (result.isAddedAbstractMethodForNonAbstractClass) {
-                    changedAbstractClasses.add(newClassNode)
-                }
+            changedMethodRef.addAll(result.effectMethods)
+            changedFieldRef.addAll(result.deletedFields)
+            if (result.isAddedAbstractMethodForNonAbstractClass) {
+                changedAbstractClasses.add(newClassNode)
             }
         }
 
