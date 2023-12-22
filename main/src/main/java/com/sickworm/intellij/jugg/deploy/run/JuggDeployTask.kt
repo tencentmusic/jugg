027985ca86474db8131c330534448fc063a84f7c
sickworm <ch.operation@gmail.com>
2023-12-22 16:07:55 +0800

    [optimize] only print warning when install

--- main/src/main/java/com/sickworm/intellij/jugg/deploy/run/JuggDeployTask.kt
+++ main/src/main/java/com/sickworm/intellij/jugg/deploy/run/JuggDeployTask.kt
@@ -63,7 +63,11 @@ class JuggDeployTask(
         // only print warning and error
         val logger = object : LogWrapper(logger) {
             init {
-                alwaysLogAsDebug(false)
+                if (type == AndroidDeployType.INSTALL) {
+                    alwaysLogAsDebug(false)
+                } else {
+                    alwaysLogAsDebug(true)
+                }
                 allowVerbose(true)
             }
 
