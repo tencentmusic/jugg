5422d817d2c3c18db45505317f1d4259cdc7430e
sickworm <ch.operation@gmail.com>
2024-01-25 17:04:04 +0800

    [optimize] recreate database when get SQLiteException error in most situation the db file is deleted

--- main/src/main/java/com/sickworm/intellij/jugg/deploy/data/SourceFileManager.kt
+++ main/src/main/java/com/sickworm/intellij/jugg/deploy/data/SourceFileManager.kt
@@ -3,21 +3,36 @@ package com.sickworm.intellij.jugg.deploy.data
 import com.intellij.openapi.diagnostic.Logger
 import com.sickworm.intellij.jugg.logger.getInstance
 import com.sickworm.intellij.jugg.project.ChangedFile
+import org.sqlite.SQLiteException
 import java.io.File
-import java.sql.DriverManager
 
 class SourceFileManager(private val logger: Logger, dbDir: File) {
 
     private val database = SourceFileDatabaseSqLiteHelper(File(dbDir, "source_files.db"), logger.getInstance("SourceFileDatabaseSqLiteHelper"))
 
+    private var sourceDirs = emptyList<File>()
+
+    private var isCanRecreateOnError = true
+
     @Synchronized
     fun init(sourceDirs: List<File>) {
+        this.sourceDirs = sourceDirs
+
         val startTime = System.currentTimeMillis()
         try {
             database.init()
             database.updateSourceDirs(sourceDirs)
+            isCanRecreateOnError = true
         } catch (e: Exception) {
             logger.warn("init error", e)
+            if (isCanRecreateOnError && (e is SQLiteException)) {
+                isCanRecreateOnError = false
+                logger.debug("get SQLiteException on init, recreate database")
+                database.recreateDatabase()
+                init(sourceDirs)
+            } else {
+                isCanRecreateOnError = false
+            }
         }
         val costTime = System.currentTimeMillis() - startTime
         logger.debug("source file db init cost ${costTime}ms")
@@ -30,6 +45,11 @@ class SourceFileManager(private val logger: Logger, dbDir: File) {
             database.updateFiles(addFiles, deleteFiles)
         } catch (e: Exception) {
             logger.warn("updateFiles error", e)
+            if (isCanRecreateOnError && (e is SQLiteException)) {
+                logger.debug("get SQLiteException on updateFiles, recreate database")
+                database.recreateDatabase()
+                init(sourceDirs)
+            }
         }
         val costTime = System.currentTimeMillis() - startTime
         if (costTime >= 100) {
