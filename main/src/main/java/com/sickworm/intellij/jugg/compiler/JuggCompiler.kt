74fea4f453592427ee1fcf677aa36c2cedfb9a81
sickworm <ch.operation@gmail.com>
2024-03-16 16:12:15 +0800

    [bugfix] fix NoSuchFieldError when invoking R.id.* after adding a new id and using kotlinx.android.synthetic (KAE)

--- main/src/main/java/com/sickworm/intellij/jugg/compiler/JuggCompiler.kt
+++ main/src/main/java/com/sickworm/intellij/jugg/compiler/JuggCompiler.kt
@@ -160,7 +160,7 @@ class JuggCompiler(
                 )
             }
             // we need to add R.dex to compile result, because R.styleable fields won't inlined
-            // other classes it's ok to not deployed, but I prefer to deploy all classes for consistency
+            // and R.* won't inline if using kotlinx.android.synthetic
             compileResult += rDexResult.copy(task = task)
         }
 
