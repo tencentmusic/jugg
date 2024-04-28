ca23a0687d5973211bfe1ebb79fbc258dc68d3c2
sickworm <ch.operation@gmail.com>
2024-04-28 16:31:50 +0800

    [optimize] retry Init project info after failed

--- main/src/main/java/com/sickworm/intellij/jugg/JuggManager.kt
+++ main/src/main/java/com/sickworm/intellij/jugg/JuggManager.kt
@@ -468,6 +468,7 @@ class JuggManager @TestOnly constructor(
     }
 
     private var currentIndicator: ProgressIndicator? = null
+    private var retryGapMill = 5_000L
 
     private fun runTaskSafe(jobName: String, action: Runnable, isNeedShowIndicator: Boolean = true) {
         object : Task.Backgroundable(project, jobName, false) {
@@ -502,6 +503,20 @@ class JuggManager @TestOnly constructor(
                     reportEventData.action = jobName
                     reportEventData.costTime = System.currentTimeMillis() - startTime
                     juggServer.report(reportEventData)
+
+                    if (!reportEventData.isSuccess) {
+                        if (jobName == "Init project info") {
+                            // compatible with com.intellij.serviceContainer.AlreadyDisposedException: Already disposed: Module: 'xxx' (disposed)
+                            logger.debug("retry $jobName after ${retryGapMill}ms") // maybe
+                            launch {
+                                delay(retryGapMill)
+                                retryGapMill *= 2
+                                runTaskSafe(jobName, action, isNeedShowIndicator)
+                            }
+                        }
+                    } else {
+                        retryGapMill = 5_000L
+                    }
                 }
             }
         }.setCancelText("Jugg: Stopping $jobName...").queue()
