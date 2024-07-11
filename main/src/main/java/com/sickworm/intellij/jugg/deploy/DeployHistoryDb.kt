5f2275d2ff7139c224b059d26bef3e56140fce14
sickworm <ch.operation@gmail.com>
2024-07-11 19:10:04 +0800

    [bugfix] fix local AndroidManifest.xml not filtered

--- main/src/main/java/com/sickworm/intellij/jugg/deploy/DeployHistoryDb.kt
+++ main/src/main/java/com/sickworm/intellij/jugg/deploy/DeployHistoryDb.kt
@@ -264,6 +264,7 @@ class DeployHistoryDb(
             gitFileMap[File(subDir)] = mutableListOf()
         }
 
+        logger.debug("filterUnchangedFiles filtering ${files.map { it.name }} in ${gitFileMap.map { it.key.path }}")
         val unchangedFiles = mutableListOf<File>()
         files.forEach { file ->
             val path = file.relativeTo(projectDir).path
@@ -292,11 +293,12 @@ class DeployHistoryDb(
 
         gitFileMap.forEach { (projectDir, files) ->
             if (files.isEmpty()) {
+                logger.debug("filterUnchangedFiles filtering [] in $projectDir")
                 return@forEach
             }
 
             val gitManager = GitManager.createGitManagerAndTrySearchParent(projectDir)
-            logger.debug("filterUnchangedFiles filtering ${gitManager.rootDir} in ${files.map { it.name }}")
+            logger.debug("filterUnchangedFiles filtering ${files.map { it.name }} in ${gitManager.rootDir}")
             val changedFileByGit = gitManager.filterChangedFiles(gitCommitMap[projectDir]!!, files).map {
                 it.absolutePath
             }.toSet()
