211178ead921fbe149fcedb994aadb0170c11f8d
sickworm <ch.operation@gmail.com>
2024-08-08 14:12:02 +0800

    [bugfix] fix DexPathListFixer has low probability wrong detect no need fix on HarmonyOS 4.2 solution: add fix flag directly when detect HarmonyOS 4.2

--- jvmti_agent/src/main/script/jugg_agent_setup.sh
+++ jvmti_agent/src/main/script/jugg_agent_setup.sh
@@ -2,6 +2,37 @@
 
 # must run by run-as {packageName}
 
+# greater=1, less=-1, equals=0, invalid=-2
+compareVersion() {
+    local regex="^[0-9]+\.[0-9]+\.?.*$"
+	if ! echo "$1" | grep -qE "$regex" ; then
+    echo "-2"
+    return
+  fi
+
+  # get first two fields of version
+  version1_major=$(echo $1 | cut -d'.' -f1)
+  version1_minor=$(echo $1 | cut -d'.' -f2)
+
+  version2_major=$(echo $2 | cut -d'.' -f1)
+  version2_minor=$(echo $2 | cut -d'.' -f2)
+
+  # 比较版本号的前两个字段
+  if [[ $version1_major -gt $version2_major ]]; then
+    echo "1"
+  elif [[ $version1_major -lt $version2_major ]]; then
+    echo "-1"
+  else
+    if [[ $version1_minor -gt $version2_minor ]]; then
+      echo "1"
+    elif [[ $version1_minor -lt $version2_minor ]]; then
+      echo "-1"
+    else
+      echo "0"
+    fi
+  fi
+}
+
 version=$1
 instrumentsJarPath="/data/local/tmp/jugg/$version/jugg-instruments.jar"
 agentSoName="jugg_jvmti_agent.so"
@@ -30,6 +61,18 @@ else
   rm $agentDir/*-$agentSoName
   # push new agent
   cp $agentSoOriginPath $agentSoDestPath
+
+  # auto setup fix flag for HarmonyOS
+  harmonyOsVersion=$(getprop hw_sc.build.platform.version)
+  # set need fix if >= 4.2
+  compareResult=$(compareVersion "$harmonyOsVersion" "4.2")
+  if [ $compareResult -eq 1 ] || [ $compareResult -eq 0 ] ; then
+    echo "need auto setup fix"
+    touch code_cache/.need_fix_dex_path_list
+  else
+    echo "no need auto setup fix"
+  fi
 fi
+
 
 echo "push success"
