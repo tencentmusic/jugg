4e801a5f5a167bf220ed0dd3f63fdcf89da5eac6
sickworm <ch.operation@gmail.com>
2025-01-15 17:38:17 +0800

    [bugfix] fix compile error with Kotlin 1.4: Symbol is declared in module 'java.base' which does not export package 'kotlin'

--- main/src/main/java/com/sickworm/intellij/jugg/gradle/script/Utils.kt
+++ main/src/main/java/com/sickworm/intellij/jugg/gradle/script/Utils.kt
@@ -1,16 +1,10 @@
 package com.sickworm.intellij.jugg.gradle.script
 
-import java.io.ByteArrayOutputStream
-import java.io.PrintStream
-
 fun printException(e: Throwable) {
-    val outputStream = ByteArrayOutputStream()
-    e.printStackTrace(PrintStream(outputStream))
-    val result = outputStream.toString()
-    val lines = result.split('\n')
-    lines.forEach { line ->
-        if (line.contains(".gradle.kts:")) {
-            println(line)
+    val stackTrace = e.stackTrace
+    stackTrace.forEach {
+        if (it.fileName?.contains(".gradle.kts") == true) {
+            println(it)
         }
     }
 }
