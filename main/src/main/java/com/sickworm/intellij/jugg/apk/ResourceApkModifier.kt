tencentmusic/jugg

commit 5b6cbfd36cadf993b4a442897f232b2665fd2ed5

[feature] [WIP] supports dynamic feature [feature] 4. handles multiple apks for resource and so deployment

main/src/main/java/com/sickworm/intellij/jugg/apk/ResourceApkModifier.kt
@@ -13,6 +13,7 @@ import java.util.zip.ZipOutputStream
 
 
 class ResourceApkModifier(
+    private val originApkPath: String,
     private val resourceApkFile: File,
     logger: Logger,
 ) {
@@ -53,7 +54,8 @@ class ResourceApkModifier(
             BuildConfig.RESOURCE_APK_NAME,
             CompileOutput.Type.Asset,
             crc32,
-            content
+            content,
+            originApkPath,
         )
         return listOf(deployItem)
     }

idea/src/main/java/com/sickworm/intellij/jugg/deploy/run/OverlayUpdateBuilder.kt
@@ -3,7 +3,6 @@ package com.sickworm.intellij.jugg.deploy.run
 import com.android.tools.deployer.DeployerException
 import com.android.tools.deployer.DeploymentCacheDatabase
 import com.android.tools.deployer.DexComparator.ChangedClasses
-import com.sickworm.intellij.jugg.project.JuggException
 
 class OverlayUpdateBuilder {
 
@@ -21,8 +20,15 @@ class OverlayUpdateBuilder {
         }
         val dexOverlays = ChangedClasses(newClasses, modifiedClasses)
 
+        val baseApk = cacheEntry.apks.find { it.name == "base.apk" } ?: cacheEntry.apks.first()
+        val cacheEntryMap = cacheEntry.apks.associateBy { it.path }
         val overlayFiles = data.overlays.associate {
-            it.toIncompleteOverlay(cacheEntry.apks.first())
+            val apk = if (it.apkPath == DeployItem.FLAG_CLASS || it.apkPath == DeployItem.FLAG_BASE_APK) {
+                baseApk
+            } else {
+                cacheEntryMap[it.apkPath] ?: baseApk
+            }
+            it.toIncompleteOverlay(apk)
         }
 
         return JuggOverlayUpdate(cacheEntry, dexOverlays, overlayFiles)

idea/src/test/java/com/sickworm/intellij/jugg/deploy/data/DeployDataGeneratorTest.kt
@@ -433,7 +433,7 @@ class DeployDataGeneratorTest {
         return ParsedDex(
             parsedApk.classes.filter { it.key == classSigName }.map {
                 ClassDeployItem(
-                    DeployItem(it.key, CompileOutput.Type.Dex, 0, byteArrayOf()),
+                    DeployItem(it.key, CompileOutput.Type.Dex, 0, byteArrayOf(), DeployItem.FLAG_CLASS),
                     listOf(it.value),
                 )
             },
@@ -507,7 +507,7 @@ class DeployDataGeneratorTest {
             return ParsedDex(
                 classDeployItems = this.classes.values.map {
                     ClassDeployItem(
-                        DeployItem(it.className, CompileOutput.Type.Dex, 0, byteArrayOf()),
+                        DeployItem(it.className, CompileOutput.Type.Dex, 0, byteArrayOf(), DeployItem.FLAG_CLASS),
                         listOf(it),
                     )
                 },

main/src/main/java/com/sickworm/intellij/jugg/compiler/ICompiler.kt
@@ -102,6 +102,7 @@ data class CompileOutput(
     val type: Type,
     val file: File,
     val baseDir: File,
+    val apkPath: String? = null,
 ) {
 
     val relativeFile get() = file.absoluteFile.relativeTo(baseDir)

main/src/main/java/com/sickworm/intellij/jugg/deploy/DeployFileManager.kt
@@ -16,6 +16,7 @@ import com.sickworm.intellij.jugg.gradle.compile.isChild
 import com.sickworm.intellij.jugg.jvmti_agent.BuildConfig
 import com.sickworm.intellij.jugg.logger.TimeLogger
 import com.sickworm.intellij.jugg.logger.getInstance
+import com.sickworm.intellij.jugg.project.JuggInternalException
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 import org.jetbrains.annotations.TestOnly
@@ -58,7 +59,11 @@ class DeployFileManager(
      */
     private val deployDataGenerator = DeployDataGenerator(logger.getInstance("DeployDataGenerator"), databaseDir)
 
-    private val resourceApkGenerator = ResourceApkGenerator(deployDataGenerator.deployDataDatabase, databaseDir, logger)
+    private val resourceApkGenerator = ResourceApkGenerator(
+        deployDataGenerator.deployDataDatabase,
+        databaseDir.resolve("resource_apks"),
+        logger,
+    )
 
     /**
      * get source file by source file name in dex file
@@ -220,7 +225,8 @@ class DeployFileManager(
                     it.update(ByteArray(0))
                     it.value
                 },
-                content = ByteArray(0)
+                content = ByteArray(0),
+                apkPath = DeployItem.FLAG_BASE_APK,
             )
             compatDeployData = compatDeployData.copy(overlays = compatDeployData.overlays + enableFlag)
         }
@@ -540,7 +546,20 @@ fun CompileOutput.toDeployItem(): DeployItem {
         update(bytes)
         value
     }
-    return DeployItem(deployItemName, type, crc, bytes)
+    when (type) {
+        CompileOutput.Type.Dex -> {
+            return DeployItem(deployItemName, type, crc, bytes, DeployItem.FLAG_CLASS)
+        }
+        CompileOutput.Type.Res, CompileOutput.Type.Asset, CompileOutput.Type.NativeLib -> {
+            if (apkPath == null) {
+                throw JuggInternalException.outputDidNotSpecificApkPath(this.toString())
+            }
+            return DeployItem(deployItemName, type, crc, bytes, apkPath)
+        }
+        else -> {
+            return DeployItem(deployItemName, type, crc, bytes, DeployItem.FLAG_BASE_APK) // will not apply to device
+        }
+    }
 }
 
 val CompileOutput.deployItemName: String get() {

main/src/main/java/com/sickworm/intellij/jugg/deploy/data/ApkParser.kt
@@ -99,6 +99,7 @@ class ApkParser: CoroutineScope by CoroutineScope(
                 CompileOutput.Type.Dex,
                 crc,
                 content,
+                DeployItem.FLAG_CLASS,
             )
         }
         return parseDex(deployItems)

main/src/main/java/com/sickworm/intellij/jugg/deploy/data/ResourceApkGenerator.kt
@@ -9,6 +9,7 @@ import com.sickworm.intellij.jugg.deploy.toDeployItem
 import com.sickworm.intellij.jugg.jvmti_agent.BuildConfig
 import com.sickworm.intellij.jugg.logger.TimeLogger
 import com.sickworm.intellij.jugg.logger.getInstance
+import com.sickworm.intellij.jugg.project.JuggInternalException
 import java.io.File
 
 /**
@@ -16,19 +17,34 @@ import java.io.File
  */
 class ResourceApkGenerator(
     private val deployDataDatabase: IDeployDataDatabase,
-    databaseDir: File,
+    private val resourceApkDir: File,
     logger: Logger,
 ) {
     private val logger = logger.getInstance("ResourceApkGenerator")
 
-    private var resourceApkFile = File(databaseDir, BuildConfig.RESOURCE_APK_NAME)
-
     fun getResourceApkDeployItem(changedOverlays: List<DeployItem>, deployedFiles: Map<String, CompileOutput>): List<DeployItem> {
+        resourceApkDir.mkdirs()
+        val changedOverlaysMap = changedOverlays.groupBy { it.apkPath }
+        return changedOverlaysMap.flatMap { (apkPath, deployItems) ->
+            if (apkPath == DeployItem.FLAG_BASE_APK || apkPath == DeployItem.FLAG_CLASS) {
+                throw JuggInternalException.flagApkPathNotAllowed(deployItems.joinToString { it.name })
+            }
+            val resourceApkName = resourceApkDir.resolve(File(apkPath).name).resolve(BuildConfig.RESOURCE_APK_NAME)
+            getResourceApkDeployItem(apkPath, resourceApkName, deployItems, deployedFiles)
+        }
+    }
+
+    private fun getResourceApkDeployItem(
+        originApkPath: String,
+        resourceApkFile: File,
+        changedOverlays: List<DeployItem>,
+        deployedFiles: Map<String, CompileOutput>,
+    ): List<DeployItem> {
         val isApkExists = resourceApkFile.exists()
-        logger.debug("getResourceApkDeployItem, isApkExists: $isApkExists")
+        logger.debug("getResourceApkDeployItem, resourceApkFile: ${resourceApkFile}, isApkExists: $isApkExists")
         TimeLogger.start("getResourceApkDeployItem")
 
-        val resourceModifier = ResourceApkModifier(resourceApkFile, logger)
+        val resourceModifier = ResourceApkModifier(originApkPath, resourceApkFile, logger)
         if (!isApkExists) {
             val nameSet = mutableSetOf<String>()
             val deployedItems = mutableListOf<DeployItem>()
@@ -61,6 +77,6 @@ class ResourceApkGenerator(
     }
 
     fun deleteResourceApk() {
-        resourceApkFile.delete()
+        resourceApkDir.deleteRecursively()
     }
 }

main/src/main/java/com/sickworm/intellij/jugg/deploy/run/JuggDeployData.kt
@@ -10,7 +10,6 @@ import com.sickworm.intellij.jugg.compiler.ClassNode
 import com.sickworm.intellij.jugg.deploy.data.EffectedClassNode
 import com.sickworm.intellij.jugg.deploy.data.ParsedDex
 import com.sickworm.intellij.jugg.deploy.outerClassName
-import kotlin.math.min
 
 /**
  * Final data that going to deploy to the device.
@@ -204,6 +203,7 @@ open class DeployItem(
     val type: CompileOutput.Type,
     val checksum: Long, // crc
     val content: ByteArray,
+    val apkPath: String, // resource belongs to which apk
 ) {
 
     fun toIncompleteOverlay(apk: Apk): Pair<ApkEntry, ByteString> {
@@ -214,6 +214,11 @@ open class DeployItem(
 
     override fun toString(): String {
         return "$type:$name"
+    }
+
+    companion object {
+        const val FLAG_CLASS = "jugg_class_flag"
+        const val FLAG_BASE_APK = "jugg_all_apk_flag"
     }
 }
 

commit 4a6379a76e6b919ec382287c7364860d427ba1d6

[bugfix] fix compat deploy failed by directory not create caused by 5b6cbfd3

main/src/main/java/com/sickworm/intellij/jugg/apk/ResourceApkModifier.kt
@@ -22,6 +22,7 @@ class ResourceApkModifier(
 
     fun createResourceApk(overlays: List<DeployItem>) {
         resourceApkFile.delete()
+        resourceApkFile.parentFile.mkdirs()
         resourceApkFile.createNewFile()
         ZipOutputStream(resourceApkFile.outputStream()).use { os ->
             val overlay = overlays.first() // must include one entry to create a normal ZIP file