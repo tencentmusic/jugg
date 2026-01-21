19748e00f323b37090009964b8ef16bec16931e1
sickworm <ch.operation@gmail.com>
2026-01-21 11:14:56 +0800

    [bugfix] fix isDebuggable not correct when has split apk reason: split apk don't have debuggable flag

--- main/src/main/java/com/sickworm/intellij/jugg/compiler/ICompiler.kt
+++ main/src/main/java/com/sickworm/intellij/jugg/compiler/ICompiler.kt
@@ -281,7 +281,7 @@ interface ICompileContext {
 
     val isReleaseApk get() = applicationModule?.buildVariant?.contains("release", ignoreCase = true) == true
 
-    val isDebuggable get() = apkInfos.flatMap { it.files }.all { it.debuggable }
+    val isDebuggable get() = apkInfos.flatMap { it.files }.filter { it.isBaseApk }.all { it.debuggable }
 
     val mappingFile get() = applicationModule?.buildPathInfo?.mappingFile
 
